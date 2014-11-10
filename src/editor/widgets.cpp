030d26421b1ba448e41208ce5da00076c5c3afaf
Sterling Orsten <sgorsten@gmail.com>
2014-11-10 08:18:14 -0800

    Simplify how splitters work.

--- src/editor/widgets.cpp
+++ src/editor/widgets.cpp
@@ -51,27 +51,29 @@ void ListBox::AddItem(const GuiFactory & factory, const std::string & text)
 // Splitters //
 ///////////////
 
-class NSSplitterBorder : public gui::Element
+struct DimensionDesc { int int2::*coord, gui::Rect::*r0, gui::Rect::*r1; gui::UCoord gui::URect::*u0, gui::URect::*u1; gui::Cursor cursor; };
+class SplitterBorder : public gui::Element
 {
     class Dragger : public gui::IDragger
     {
         gui::Element & panel;
         gui::URect initialPlacement;
-        int clickY;
+        DimensionDesc dim;
+        int click;
     public:
-        Dragger(gui::Element & panel, const int2 & click) : panel(panel), initialPlacement(panel.children[0].placement), clickY(click.y) {}
+        Dragger(gui::Element & panel, const DimensionDesc & dim, const int2 & click) : panel(panel), initialPlacement(panel.children[0].placement), dim(dim), click(click.*dim.coord) {}
         void OnDrag(int2 newMouse) override
         {
             auto borderRect = initialPlacement.resolve(panel.rect);
-            int dy = newMouse.y - clickY;
-            dy = std::max(dy, 100 - borderRect.y0);
-            dy = std::min(dy, panel.rect.GetHeight() - 100 - borderRect.y1);
+            int delta = newMouse.*dim.coord - click;
+            delta = std::max(delta, panel.rect.*dim.r0 + 100 - borderRect.*dim.r0);
+            delta = std::min(delta, panel.rect.*dim.r1 - 100 - borderRect.*dim.r1);
             auto placement = initialPlacement;
-            placement.y0.b += dy;
-            placement.y1.b += dy;
+            (placement.*dim.u0).b += delta;
+            (placement.*dim.u1).b += delta;
             panel.children[0].placement = placement;
-            panel.children[1].placement.y1 = placement.y0;
-            panel.children[2].placement.y0 = placement.y1;
+            panel.children[1].placement.*dim.u1 = placement.*dim.u0;
+            panel.children[2].placement.*dim.u0 = placement.*dim.u1;
             panel.SetRect(panel.rect);
         }
         void OnRelease() override {}
@@ -79,15 +81,16 @@ class NSSplitterBorder : public gui::Element
     };
 
     gui::Element & panel;
+    DimensionDesc dim;
 public:
-    NSSplitterBorder(gui::Element & panel) : panel(panel) { cursor = gui::Cursor::SizeNS; style = gui::BACKGROUND; }
-    gui::DraggerPtr OnClick(int x, int y) override { return std::make_shared<Dragger>(panel, int2(x,y)); }
+    SplitterBorder(gui::Element & panel, const DimensionDesc & dim) : panel(panel), dim(dim) { cursor = dim.cursor; style = gui::BACKGROUND; }
+    gui::DraggerPtr OnClick(int x, int y) override { return std::make_shared<Dragger>(panel, dim, int2(x,y)); }
 };
 
 gui::ElementPtr GuiFactory::MakeNSSizer(gui::ElementPtr top, gui::ElementPtr bottom, int split)
 {
     auto panel = std::make_shared<gui::Element>();
-    auto sizer = std::make_shared<NSSplitterBorder>(*panel);
+    auto sizer = std::make_shared<SplitterBorder>(*panel, DimensionDesc{&int2::y, &gui::Rect::y0, &gui::Rect::y1, &gui::URect::y0, &gui::URect::y1, gui::Cursor::SizeNS});
     int height = 4;
     if(split > 0) panel->children.push_back({{{0,0},{0,split},{1,0},{0,split+height}}, sizer});
     if(split < 0) panel->children.push_back({{{0,0},{1,split-height},{1,0},{1,split}}, sizer});
@@ -97,43 +100,10 @@ gui::ElementPtr GuiFactory::MakeNSSizer(gui::ElementPtr top, gui::ElementPtr bot
     return panel;
 }
 
-class WESplitterBorder : public gui::Element
-{
-    class Dragger : public gui::IDragger
-    {
-        gui::Element & panel;
-        gui::URect initialPlacement;
-        int clickX;
-    public:
-        Dragger(gui::Element & panel, const int2 & click) : panel(panel), initialPlacement(panel.children[0].placement), clickX(click.x) {}
-        void OnDrag(int2 newMouse) override
-        {
-            auto borderRect = initialPlacement.resolve(panel.rect);
-            int dx = newMouse.x - clickX;
-            dx = std::max(dx, 100 - borderRect.x0);
-            dx = std::min(dx, panel.rect.GetWidth() - 100 - borderRect.x1);
-            auto placement = initialPlacement;
-            placement.x0.b += dx;
-            placement.x1.b += dx;
-            panel.children[0].placement = placement;
-            panel.children[1].placement.x1 = placement.x0;
-            panel.children[2].placement.x0 = placement.x1;
-            panel.SetRect(panel.rect);
-        }
-        void OnRelease() override {}
-        void OnCancel() override {}
-    };
-
-    gui::Element & panel;
-public:
-    WESplitterBorder(gui::Element & panel) : panel(panel) { cursor = gui::Cursor::SizeWE; style = gui::BACKGROUND; }
-    gui::DraggerPtr OnClick(int x, int y) override { return std::make_shared<Dragger>(panel, int2(x,y)); }
-};
-
 gui::ElementPtr GuiFactory::MakeWESizer(gui::ElementPtr left, gui::ElementPtr right, int split)
 {
     auto panel = std::make_shared<gui::Element>();
-    auto sizer = std::make_shared<WESplitterBorder>(*panel);
+    auto sizer = std::make_shared<SplitterBorder>(*panel, DimensionDesc{&int2::x, &gui::Rect::x0, &gui::Rect::x1, &gui::URect::x0, &gui::URect::x1, gui::Cursor::SizeWE});
     int width = 4;
     if(split > 0) panel->children.push_back({{{0,split},{0,0},{0,split+width},{1,0}}, sizer});
     if(split < 0) panel->children.push_back({{{1,split-width},{0,0},{1,split},{1,0}}, sizer});
